cccd2c120235eb0fa6c976a467b832055ce6cb10
hykinfactorio <hykinfactorio@gmail.com>
2026-05-13 09:40:31 -0400

    Update viewer_config.js

--- viewer/viewer_config.js
+++ viewer/viewer_config.js
@@ -1,10 +1,10 @@
 const viewerConfig = {
   planetPreviewSources: {
-    nauvis: "https://drive.usercontent.google.com/download?export=view&id=1eWIArFivdD-aedHT2cwgMrHsQtTQp7SF",
-    vulcanus: "https://drive.usercontent.google.com/download?export=view&id=1AmlipIwo3siD2Jyw93vwAfRoKQJbMv18",
-    gleba: "https://drive.usercontent.google.com/download?export=view&id=1GNVTzlELuxOVIkAQLAg0rNiDsJr2jqbi",
-    fulgora: "https://drive.usercontent.google.com/download?export=view&id=1D_ofEHza2cIn6RJjU-fLi3UJ3cAkURvm",
-    aquilo: "https://drive.usercontent.google.com/download?export=view&id=1_qV6Pe2znQReopC0Wq1awDEXVUreEZiT",
+    nauvis: "https://dl.dropboxusercontent.com/scl/fi/x1whpxh0aftpkm36vld4w/nauvis.png?rlkey=hsnocz6bhac2osudw7avix67x",
+    vulcanus: "https://dl.dropboxusercontent.com/scl/fi/69lmlvyqv6za4xykhmmts/vulcanus.png?rlkey=t67d651tbosnrg5l4erpxkvz5",
+    gleba: "https://dl.dropboxusercontent.com/scl/fi/qpeywcysz7pmv2odmvyra/gleba.png?rlkey=07lbkqn2w4daxikz61gxrulm4",
+    fulgora: "https://dl.dropboxusercontent.com/scl/fi/gwuy591hmzw5c7cyz3hw2/fulgora.png?rlkey=wymb81gjwxh5qfyjvn7qlafr9",
+    aquilo: "https://dl.dropboxusercontent.com/scl/fi/re803d59b5zktwz9afldr/aquilo.png?rlkey=9uvcgx8aa1hj07859toi07ng3",
   },
-  planetNamesSource: "https://hykinfactorio.github.io/FactorioPreviewToolkit/previews/local_planet_names.js"
+  planetNamesSource: "https://dl.dropboxusercontent.com/scl/fi/cnm17khsugnqvpkwo09s5/remote_planet_names.json?rlkey=h5wu60jrgu6cl9zcof4hjxaj9"
 };
