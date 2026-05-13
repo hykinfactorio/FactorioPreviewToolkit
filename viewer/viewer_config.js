89bad1fea519c75cea458943534c014e5a27fa91
hykinfactorio <hykinfactorio@gmail.com>
2026-05-13 09:25:38 -0400

    Update viewer_config.js

--- viewer/viewer_config.js
+++ viewer/viewer_config.js
@@ -1,6 +1,6 @@
 const viewerConfig = {
   planetPreviewSources: {
-    nauvis: "https://drive.google.com/uc?export=view&id=id=1eWIArFivdD-aedHT2cwgMrHsQtTQp7SF",
+    nauvis: "https://drive.google.com/uc?export=view&id=1eWIArFivdD-aedHT2cwgMrHsQtTQp7SF",
     vulcanus: "https://drive.usercontent.google.com/download?id=1AmlipIwo3siD2Jyw93vwAfRoKQJbMv18",
     gleba: "https://drive.usercontent.google.com/download?id=1GNVTzlELuxOVIkAQLAg0rNiDsJr2jqbi",
     fulgora: "https://drive.usercontent.google.com/download?id=1D_ofEHza2cIn6RJjU-fLi3UJ3cAkURvm",
