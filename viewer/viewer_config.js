ba2c66771b5cb15c3ff82051d9bb92d8632c7da9
hykinfactorio <hykinfactorio@gmail.com>
2026-05-13 09:01:31 -0400

    Update viewer_config.js

--- viewer/viewer_config.js
+++ viewer/viewer_config.js
@@ -1,10 +1,10 @@
-const viewerConfig  = {
-    planetPreviewSources: {
-    nauvis: "../previews/nauvis.png",
-    vulcanus: "../previews/vulcanus.png",
-    gleba: "../previews/gleba.png",
-    fulgora: "../previews/fulgora.png",
-    aquilo: "../previews/aquilo.png"
+const viewerConfig = {
+  planetPreviewSources: {
+    nauvis: "https://drive.google.com/open?id=1eWIArFivdD-aedHT2cwgMrHsQtTQp7SF",
+    vulcanus: "https://drive.google.com/open?id=1AmlipIwo3siD2Jyw93vwAfRoKQJbMv18",
+    gleba: "https://drive.google.com/open?id=1GNVTzlELuxOVIkAQLAg0rNiDsJr2jqbi",
+    fulgora: "https://drive.google.com/open?id=1D_ofEHza2cIn6RJjU-fLi3UJ3cAkURvm",
+    aquilo: "https://drive.google.com/open?id=1_qV6Pe2znQReopC0Wq1awDEXVUreEZiT",
   },
-  planetNamesSource: "../previews/local_planet_names.js"
+  planetNamesSource: "https://drive.google.com/open?id=1S9OjtYe8icc8PK-iNGAigXhPJAW-x-Oq"
 };
