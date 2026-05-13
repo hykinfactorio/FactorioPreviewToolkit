1a2b12feb7bb17cc4942c1dc0b87c51ef7c35dff
hykinfactorio <hykinfactorio@gmail.com>
2026-05-13 09:05:53 -0400

    Update viewer_config.js

--- viewer/viewer_config.js
+++ viewer/viewer_config.js
@@ -1,10 +1,10 @@
 const viewerConfig = {
   planetPreviewSources: {
-    nauvis: "https://drive.google.com/open?id=1eWIArFivdD-aedHT2cwgMrHsQtTQp7SF",
-    vulcanus: "https://drive.google.com/open?id=1AmlipIwo3siD2Jyw93vwAfRoKQJbMv18",
-    gleba: "https://drive.google.com/open?id=1GNVTzlELuxOVIkAQLAg0rNiDsJr2jqbi",
-    fulgora: "https://drive.google.com/open?id=1D_ofEHza2cIn6RJjU-fLi3UJ3cAkURvm",
-    aquilo: "https://drive.google.com/open?id=1_qV6Pe2znQReopC0Wq1awDEXVUreEZiT",
+    nauvis: "https://drive.usercontent.google.com/download?id=1eWIArFivdD-aedHT2cwgMrHsQtTQp7SF",
+    vulcanus: "https://drive.usercontent.google.com/download?id=1AmlipIwo3siD2Jyw93vwAfRoKQJbMv18",
+    gleba: "https://drive.usercontent.google.com/download?id=1GNVTzlELuxOVIkAQLAg0rNiDsJr2jqbi",
+    fulgora: "https://drive.usercontent.google.com/download?id=1D_ofEHza2cIn6RJjU-fLi3UJ3cAkURvm",
+    aquilo: "https://drive.usercontent.google.com/download?id=1_qV6Pe2znQReopC0Wq1awDEXVUreEZiT",
   },
-  planetNamesSource: "https://drive.google.com/open?id=1S9OjtYe8icc8PK-iNGAigXhPJAW-x-Oq"
+  planetNamesSource: "https://drive.usercontent.google.com/download?id=1S9OjtYe8icc8PK-iNGAigXhPJAW-x-Oq"
 };
