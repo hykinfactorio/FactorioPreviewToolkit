91d71b63723b4c252941eb79edf4c53293bd3897
hykinfactorio <hykinfactorio@gmail.com>
2026-05-13 09:22:50 -0400

    Update viewer_config.js

--- viewer/viewer_config.js
+++ viewer/viewer_config.js
@@ -1,10 +1,10 @@
 const viewerConfig = {
   planetPreviewSources: {
-    nauvis: "https://drive.usercontent.google.com/download?id=1eWIArFivdD-aedHT2cwgMrHsQtTQp7SF",
+    nauvis: "https://drive.google.com/uc?export=view&id=id=1eWIArFivdD-aedHT2cwgMrHsQtTQp7SF",
     vulcanus: "https://drive.usercontent.google.com/download?id=1AmlipIwo3siD2Jyw93vwAfRoKQJbMv18",
     gleba: "https://drive.usercontent.google.com/download?id=1GNVTzlELuxOVIkAQLAg0rNiDsJr2jqbi",
     fulgora: "https://drive.usercontent.google.com/download?id=1D_ofEHza2cIn6RJjU-fLi3UJ3cAkURvm",
     aquilo: "https://drive.usercontent.google.com/download?id=1_qV6Pe2znQReopC0Wq1awDEXVUreEZiT",
   },
-  planetNamesSource: "../preview/local_planet_names.js"
+  planetNamesSource: "https://hykinfactorio.github.io/FactorioPreviewToolkit/previews/local_planet_names.js"
 };
